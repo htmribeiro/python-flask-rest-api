c6f4fd150be4cdd0f855ca3b7723515cac164977
Hamilton Ribeiro <htadeu@hotmail.com>
2021-03-19 23:27:01 -0300

    Update run.py
    Retirado os comentários e incluídos no arquivo "Módulo 02 - REST API com Flask [Ambiente]"

--- ola_flask/run.py
+++ ola_flask/run.py
@@ -1,11 +1,7 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 # Crindo um decorador
-# Podemos passar parâmetros `<numero>`
-# Essa é uma forma que temos de pegar um parâmetro atráves da URI
-# Podemos declarar o tipo do parâmetro que será passado. Ex: `<int:numero>` - Inteiro
 @app.route("/<numero>", methods=['GET','POST'])
 
 # Criando a primeira rota
@@ -14,9 +10,4 @@ def ola(numero):
 
 # Defindo a chamada desse próprio modulo
 if __name__=="__main__":
-    # Adicionando o modo `debug=True` a aplicação reinicia a cada alteração
-    # Em produção o modo `debug=False` senão ele apresentará toda a trilha do erro
     app.run(debug=True)
-
-# Ao rodarmos, a aplicação será aberta no navegador
-# Obs.: O navegador utiliza por padrão o método GET.
